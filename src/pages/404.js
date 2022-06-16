d5bf71fff46a80330395be5d3ce3e9f7f127c45d
jocnn <ijann_@outlook.com>
2022-06-16 17:34:02 -0300

    limpiando gatsby

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,8 +1,5 @@
 import * as React from "react"
 
-import Layout from "../components/layout"
-import Seo from "../components/seo"
-
 const NotFoundPage = () => (
   <Layout>
     <Seo title="404: Not found" />
