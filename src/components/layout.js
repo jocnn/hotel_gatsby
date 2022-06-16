746dd9364e54003f82027b3d9cabe9ba0fc5b6f2
jocnn <ijann_@outlook.com>
2022-06-16 19:28:14 -0300

    se agrego estilos globales usando emotion/react

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,11 +1,44 @@
 import * as React from "react"
 import { Helmet } from "react-helmet"
+import { Global, css } from "@emotion/react"
 
 import Header from "./header"
 
 const Layout = ({children}) => {
   return (
     <>
+      <Global 
+        styles={css`
+          html {
+            font-size: 62.5%;
+              box-sizing: border-box;
+            }
+            *, *:before, *:after {
+              box-sizing: inherit;
+            }
+            body {
+              font-size: 18px;
+              font-size: 1.8rem;
+              line-height: 1.5;
+              font-family: 'PT Sans', sans-serif;
+            }
+            h1, h2, h3 {
+              margin: 0;
+              line-height: 1.5;
+            }
+            h1, h2 {
+              font-family: 'Roboto', serif;
+            }
+            h3 {
+              font-family: 'PT Sans', sans-serif;
+            }
+            ul {
+              list-style: none;
+              margin: 0;
+              padding:0;
+            }
+        `}
+      />
       <Helmet>
         <link 
           rel="stylesheet" 
