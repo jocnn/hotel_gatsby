0ba193d5f43fac70f783143693950a52936079ca
jocnn <ijann_@outlook.com>
2022-06-16 17:50:41 -0300

    se creo la plantilla desde cero, se importo header

---
+++ src/components/layout.js
@@ -0,0 +1,15 @@
+import * as React from "react"
+
+import Header from "./header"
+
+const Layout = ({children}) => {
+  return (
+    <>
+      <Header />
+      
+      {children}
+    </>
+  )
+}
+
+export default Layout
