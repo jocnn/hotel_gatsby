7421bbb8b73e7fbb38a1349958aa114363c3c8e6
jocnn <ijann_@outlook.com>
2022-06-20 07:45:17 -0300

    se creo el hero del home

--- src/components/imagenHotel.js
+++ src/components/imagenHotel.js
@@ -1,5 +1,11 @@
 import * as React from "react"
 import { graphql, useStaticQuery } from "gatsby"
+import BackgroundImage from "gatsby-background-image"
+import styled from "@emotion/styled"
+
+const ImageBackground = styled(BackgroundImage)`
+  height: 700px;
+`
 
 const ImagenHotel = () => {
 
@@ -12,19 +18,48 @@ const ImagenHotel = () => {
       ) {
         sharp: childImageSharp {
           fluid {
-            srcSet
+            ...GatsbyImageSharpFluid_withWebp
           }
         }
       }
     }
   `)
 
-  console.log(image)
+  const TextImagen = styled.div`
+    background-image: linear-gradient(to top, rgba(34, 49, 63, .7), rgba(34, 49, 63, .7));
+    color: #fff;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    flex: 1;
+    align-items: center;
+    justify-content: center;
+
+    h1 {
+      font-size: 4rem;
+      margin: 0%;
+      @media (min-width: 768px) {
+        font-size: 5.8rem;
+      }
+    }
+    p {
+      font-size: 2rem;
+      @media (min-width: 768px) {
+        font-size: 2.5rem;
+      }
+    }
+  `
 
   return (
-    <>
-      
-    </>
+    <ImageBackground
+      tag="section"
+      fluid={image.sharp.fluid}
+    >
+      <TextImagen>
+        <h1>Bienvenido al Hotel Gatsby</h1>
+        <p>El mejor hotel para tus vacaciones</p>
+      </TextImagen>
+    </ImageBackground>
   )
 }
 
