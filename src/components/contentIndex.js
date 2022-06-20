fd1d8c2f4fc20d53d752ed063e7be5534babbdc3
jocnn <ijann_@outlook.com>
2022-06-20 19:06:06 -0300

    se agrego estilos a la publicación

--- src/components/contentIndex.js
+++ src/components/contentIndex.js
@@ -1,6 +1,26 @@
 import * as React from "react"
 import { graphql, useStaticQuery } from "gatsby"
 import { GatsbyImage } from "gatsby-plugin-image"
+import { css } from "@emotion/react"
+import styled from "@emotion/styled"
+
+const TextInicio = styled.div`
+  padding-top: 4rem;
+  max-width: 1200px;
+  width: 95%;
+  margin: 0 auto;
+
+  @media (min-width: 768px) {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    column-gap: 2rem;
+    margin-bottom: 2rem;
+  }
+  p {
+    line-height: 2;
+    margin-top: 0px;
+  }
+`
 
 const ContentIndex = () => {
 
@@ -24,11 +44,17 @@ const ContentIndex = () => {
 
   return (
     <>
-      <h2>{titulo}</h2>
-      <div>
+      <h2
+        css={css`
+          text-align: center;
+          font-size: 4rem;
+          margin-top: 4rem;
+        `}
+      >{titulo}</h2>
+      <TextInicio>
         <p>{contenido}</p>
         <GatsbyImage image={imagen.gatsbyImageData} alt="imagen index" />
-      </div>
+      </TextInicio>
     </>
   )
 }
