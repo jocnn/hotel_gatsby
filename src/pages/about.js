b831a054e4039decf893cfe15ff247224fd9085d
jocnn <ijann_@outlook.com>
2022-06-20 19:11:01 -0300

    se importo el contenido de about

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,13 @@
 import * as React from "react"
 
 import Layout from "../components/layout"
+import ContentAbout from "../components/contentAbout"
 
 const About = () => (
   <Layout>
-    <h1>About</h1>
+    
+    <ContentAbout />
+
   </Layout>
 )
 
