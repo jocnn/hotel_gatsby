c84b3c3e111ff3781e30c2b6bfc71030cd031eb5
jocnn <ijann_@outlook.com>
2022-06-20 10:52:35 -0300

    importando componente

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,14 @@ import * as React from "react"
 
 import Layout from "../components/layout"
 import ImagenHotel from "../components/imagenHotel"
+import ContentIndex from "../components/contentIndex"
 
 const IndexPage = () => (
   <Layout>
 
     <ImagenHotel />
+
+    <ContentIndex />
 
   </Layout>
 )
