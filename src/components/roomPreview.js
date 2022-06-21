f0bcf490f96341df1a28d966bec95cf5fcd6817a
jocnn <ijann_@outlook.com>
2022-06-21 07:47:42 -0300

    componente para cada habitacion listada

---
+++ src/components/roomPreview.js
@@ -0,0 +1,14 @@
+import * as React from "react"
+
+const RoomPreview = ({ habitacion }) => {
+
+  console.log(habitacion.titulo)
+  
+  return (
+    <>
+      <h1>Desde RoomPreview</h1>
+    </>
+  )
+}
+
+export default RoomPreview
