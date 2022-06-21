947d18ad60836b2b62310bec229c2164aadd7986
jocnn <ijann_@outlook.com>
2022-06-21 12:12:28 -0300

    consultando la api para mostrar la información

--- src/components/habitacion.js
+++ src/components/habitacion.js
@@ -1,6 +1,25 @@
 import * as React from "react"
+import { graphql } from "gatsby"
 
-const Habitacion = () => {
+export const query = graphql`
+  query($slug: String!) {
+    allDatoCmsHabitacion(filter: {slug: {eq: $slug }}) {
+      nodes {
+        slug
+        contenido
+        titulo
+        imagen {
+          gatsbyImageData(imgixParams: {maxW: 1200})
+        }
+      }
+    }
+  }
+`
+
+const Habitacion = ({ data }) => {
+
+  console.log(data)
+
   return (
     <>
       <h1>Habitacion</h1>
