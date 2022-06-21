828c80f2f54899f46405eb9562de956cb25e208d
jocnn <ijann_@outlook.com>
2022-06-21 11:51:33 -0300

    generando paginas dinamicas

--- gatsby-node.js
+++ gatsby-node.js
@@ -7,3 +7,31 @@
 //     defer: true,
 //   })
 // }
+
+exports.createPages = async ({ actions, graphql, reporter }) => {
+  const result = await graphql(`
+    query {
+      allDatoCmsHabitacion {
+        nodes {
+          slug
+        }
+      }
+    }
+  `)
+
+  if (result.errors) {
+    reporter.panic("No hubo resultados", result.errors)
+  }
+
+  const habitaciones = result.data.allDatoCmsHabitacion.nodes
+
+  habitaciones.forEach(habitacion => {
+    actions.createPage({
+      path: habitacion.slug,
+      component: require.resolve("./src/components/habitacion.js"),
+      context: {
+        slug: habitacion.slug,
+      },
+    })
+  })
+}
