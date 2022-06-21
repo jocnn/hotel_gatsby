f09df6492a46d916384cbe42ce907aa433dff5c1
jocnn <ijann_@outlook.com>
2022-06-21 07:49:23 -0300

    obteniendo las habitaciones del custom hook

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,50 @@
 import * as React from "react"
+import { css } from "@emotion/react"
 
 import Layout from "../components/layout"
 import ImagenHotel from "../components/imagenHotel"
 import ContentIndex from "../components/contentIndex"
+import RoomPreview from "../components/roomPreview"
 
-const IndexPage = () => (
-  <Layout>
+import useHabitacion from "../hooks/useHabitacion"
 
-    <ImagenHotel />
+const IndexPage = () => {
 
-    <ContentIndex />
+  const habitaciones = useHabitacion()
 
-  </Layout>
-)
+  return (
+  
+    <Layout>
+
+      <ImagenHotel />
+
+      <ContentIndex />
+
+      <div>
+        <h2
+          css={css`
+            text-align: center;
+            margin-top: 5rem;
+            font-size: 3rem;
+          `}
+        >
+          Nuestras Habitaciones
+        </h2>
+
+        <ul>
+          {
+            habitaciones.map( habitacion => (
+              <RoomPreview 
+                key={habitacion.id}
+                habitacion={habitacion}
+              />
+            ))
+          }
+        </ul>
+      </div>
+
+    </Layout>
+  )
+}
 
 export default IndexPage
